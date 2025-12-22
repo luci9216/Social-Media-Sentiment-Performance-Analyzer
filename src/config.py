66e603a10016b1953b28c9d2cfec8fc29beffa4b
nzuk3720 <k.nzurum@uleth.ca>
2025-12-21 23:48:26 -0700

    created api and linked (hidden)

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+if not YOUTUBE_API_KEY:
+    raise ValueError("Missing YOUTUBE_API_KEY.")
